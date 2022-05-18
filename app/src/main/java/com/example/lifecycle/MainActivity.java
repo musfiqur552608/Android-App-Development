49035874f85fc580e325cff9198a1ac23e7bbe49
Musfiqur Rahman <you@example.com>
2022-05-18 18:17:21 +0600

    Day 1

---
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "OnCreate()", Toast.LENGTH_SHORT).show();
+    }
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "OnRestart()", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause()", Toast.LENGTH_SHORT).show();
+    }
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume()", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop()", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy()", Toast.LENGTH_SHORT).show();
+    }
+}
