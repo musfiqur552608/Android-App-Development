3cdcf38c97d3c890cc6d1752b9ecdf0c1f40b4ee
Musfiqur Rahman <you@example.com>
2022-05-18 20:15:08 +0600

    Linear Layout done

--- app/src/main/java/com/example/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -3,42 +3,29 @@ package com.example.lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView1;
+    private EditText editText1;
+    private LinearLayout layout1;
+    private ImageView imageView1, imageView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnCreate()", Toast.LENGTH_SHORT).show();
-    }
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "OnStart()", Toast.LENGTH_SHORT).show();
+
+        textView1 = findViewById(R.id.textView1);
+        editText1 = findViewById(R.id.editText1);
+        layout1 = findViewById(R.id.linerarlayout1);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+
+        textView1.setText("Musfiqur Rahman");
     }
 
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(this, "OnRestart()", Toast.LENGTH_SHORT).show();
-    }
-
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "OnPause()", Toast.LENGTH_SHORT).show();
-    }
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "OnResume()", Toast.LENGTH_SHORT).show();
-    }
-
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "OnStop()", Toast.LENGTH_SHORT).show();
-    }
-
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "OnDestroy()", Toast.LENGTH_SHORT).show();
-    }
 }
