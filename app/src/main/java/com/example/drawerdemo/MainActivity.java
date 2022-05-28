7451cd5b1a01ed6496baffd18a4279ce0f932fbb
Musfiqur Rahman <you@example.com>
2022-05-28 20:25:20 +0600

    Drawer

--- app/src/main/java/com/example/drawerdemo/MainActivity.java
+++ app/src/main/java/com/example/drawerdemo/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.drawerdemo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navViewid);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        loadFragment(new HomeFragment());
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int i = item.getItemId();
+
+                if(i == R.id.homeId){
+                    loadFragment(new HomeFragment());
+                }
+                else if(i == R.id.profileId){
+                    loadFragment(new ProfileFragment());
+                }
+                else if(i == R.id.settingsId){
+                    loadFragment(new SettingFragment());
+                }
+                else {
+                    loadFragment(new AboutFragment());
+                }
+
+                return false;
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragment);
+        ft.commit();
     }
 }
